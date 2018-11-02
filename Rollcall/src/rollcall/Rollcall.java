3682ea1536b31e53d352a0507322e9b4bf8596c8
Emekaugo <36327185+Emekaugo@users.noreply.github.com>
2018-11-02 11:10:09 +0100

    Add files via upload

---
+++ Rollcall/src/rollcall/Rollcall.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rollcall;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class Rollcall {
+
+   
+    public static void main(String[] args) {
+        Signup cun=new Signup();
+        cun.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        cun.setSize(557,512);
+        cun.setVisible(true);
+        cun.setTitle("first");
+        
+        
+    }
+    
+}
